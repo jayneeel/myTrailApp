dfa9457b79e647c266d90e958410ad2741d365d6
Jayneel <jayneelkanungo57@gmail.com>
2023-02-11 09:55:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mytrailapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mytrailapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login, register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = findViewById(R.id.logBtn);
+        register = findViewById(R.id.regBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), com.example.mytrailapp.login.class);
+                startActivity(i);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), com.example.mytrailapp.Register.class);
+                startActivity(i);
+            }
+        });
+    }
+}
