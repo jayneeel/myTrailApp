c33cebae26d05c4a73273674d5b344f60d9a034c
Jayneel <jayneelkanungo57@gmail.com>
2023-02-11 09:57:26 +0530

    Removed Action Bar

--- app/src/main/java/com/example/mytrailapp/MainActivity.java
+++ app/src/main/java/com/example/mytrailapp/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         login = findViewById(R.id.logBtn);
         register = findViewById(R.id.regBtn);
 
